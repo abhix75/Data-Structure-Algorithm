53de91197b133c96824e166b51636420a173e15d
Abhijit Mishra <abhijitmishraak10@gmail.com>
2023-07-06 05:21:53 +0530

    algo

---
+++ 1-Binary-Search-Algo.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int binarysearch(int arr[], int n, int k) {
+       
+       int low=0,high=n-1;
+       
+       while(low<=high)
+       {
+           int mid=(low+high)/2;
+           
+           if(arr[mid]==k) return mid;
+           
+           else if(arr[mid]>k)
+           {
+               high=mid-1;
+           }
+           else
+           {
+               low=mid+1;
+           }
+       }
+       return -1;
+    }
+};
